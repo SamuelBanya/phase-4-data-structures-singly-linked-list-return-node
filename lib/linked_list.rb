6d83ea0f86dcd74794ffd615f41c2cc160d6743c
Samuel Banya <sbanya@fastmail.com>
2022-12-02 15:20:02 -0600

    commit tree 8b7b3d8d743836677aed1ab2ade257ab552fdf8d

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,29 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
+    # Write a method #nth_from_end that takes a number n and returns the value at the nth node from the end of the linked list. If the nth node does not exist, return nil.
+
+    # For example, when given a linked list 3 -> 2 -> 1:
+    # When n = 1, return 1 (the element at the end of the list)
+    # When n = 3, return 3 (the third element from the end)
+    # When n = 4, return nil (since 4 is greater than the length of the list)
+    length = 0
+    node = head
+    until node.nil?
+      node = node.next_node
+      length += 1
+    end
+
+    return nil if n < 1 || n > length
+
+    node = head 
+    i = 1
+    while i < length - n + 1
+      node = node.next_node
+      i += 1
+    end
+
+    node.value
   end
 
 end
